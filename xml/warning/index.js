4614fe792ecc1f382e664179599647424335547f
tattii <yuta.tatti@gmail.com>
2020-06-18 12:07:50 +0900

    add warning

---
+++ xml/warning/index.js
@@ -0,0 +1,77 @@
+const { PubSub } = require('@google-cloud/pubsub');
+const { Datastore } = require('@google-cloud/datastore');
+const projectId = 'weatherbox-217409';
+const datastore = new Datastore({ projectId });
+
+const warning = require('./xml')
+
+
+exports.handler = async (event, context) => {
+  const pubsubMessage = event.data;
+  const message = JSON.parse(Buffer.from(pubsubMessage, 'base64').toString());
+  console.log(message);
+
+  await main(message.url);
+};
+
+if (require.main === module) {
+  (async () => {
+    const data = await main(process.argv[2]);
+  console.log(data);
+  })();
+}
+
+
+async function main(url) {
+  const data = await warning.fetchXML(url);
+  await saveDatastore(data);
+  return data;
+}
+
+
+async function saveDatastore(data) {
+  const id = data.pref.code;
+  const entity = {
+    key: datastore.key(['jma-xml-weather-warning', id]),
+    data: [
+      {
+        name: 'datetime',
+        value: new Date(data.datetime),
+        excludeFromIndexes: true,
+      },
+      {
+        name: 'pref',
+        value: data.pref.name,
+        excludeFromIndexes: true,
+      },
+      {
+        name: 'headline',
+        value: data.headline,
+        excludeFromIndexes: true,
+      },
+      {
+        name: 'data',
+        value: data,
+        excludeFromIndexes: true,
+      },
+    ]
+  };
+
+  try {
+    await datastore.save(entity);
+    await publishUpdate({ id });
+
+  } catch (err) {
+    console.error('ERROR:', err);
+  }
+}
+
+
+async function publishUpdate(data) {
+  const topicName = 'jma-xml-warning-update';
+  const pubsub = new PubSub({projectId});
+
+  const messageId = await pubsub.topic(topicName).publish(Buffer.from(JSON.stringify(data)));
+  console.log("published: " + topicName);
+}
+
--- xml/warning/xml.js
+++ xml/warning/xml.js
@@ -3,16 +3,80 @@ const xml2js = require('xml2js');
 
 
 if (require.main === module) {
-  fetchXML(process.argv[2]);
+  (async () => {
+    const data = await fetcXMLh(process.argv[2]);
+    console.log(data);
+  })();
 }
-
 
 async function fetchXML(url) {
   const res = await fetch(url);
   const content = await res.text();
   const xml = await parseXML(content);
-  console.log(xml);
+  return parse(xml);
 }
+
+function parse(xml) {
+  if (xml.Report.Control[0].Status[0] != '通常') return;
+
+  const data = {};
+  data.type     = xml.Report.Control[0].Title[0];
+  data.office   = xml.Report.Control[0].PublishingOffice[0];
+  data.title    = xml.Report.Head[0].Title[0];
+  data.datetime = xml.Report.Head[0].ReportDateTime[0];
+  data.headline = xml.Report.Head[0].Headline[0].Text[0];
+
+  data.pref = parsePref(xml);
+  data.cities = parseCity(xml);
+  return data;
+}
+
+function parsePref(xml) {
+  const warning = xml.Report.Body[0].Warning[0];
+  const area = warning.Item[0].Area[0];
+  return { code: area.Code[0], name: area.Name[0] };
+}
+
+function parseCity(xml) {
+  const warning = xml.Report.Body[0].Warning[3];
+
+  const cities = {};
+  warning.Item.forEach(item => {
+    const code = item.Area[0].Code[0];
+    
+    cities[code] = {
+      name: item.Area[0].Name[0],
+      warnings: getWarnings(item.Kind),
+      status: getStatus(item.Kind)
+    };
+  });
+
+  return cities;
+}
+
+function getWarnings(kinds) {
+  if (kinds[0].Status[0] === '発表警報・注意報はなし') {
+    return [];
+  } else {
+    const names = kinds.map(kind => kind.Name[0])
+      .filter(name => name !== '解除');
+    return names;
+  }
+}
+
+function getStatus(kinds) {
+  if (kinds[0].Status[0] === '発表警報・注意報はなし') {
+    return 'none';
+  } else {
+    const names = kinds.map(kind => kind.Name[0]);
+    if (names.find(d => d.includes('特別警報'))) return 'emergency';
+    if (names.find(d => d.includes('警報'))) return 'warning';
+    if (names.find(d => d.includes('注意報'))) return 'advisory';
+    return 'none';
+  }
+}
+
+
 
 function parseXML(data) {
   return new Promise(function (resolve, reject) {
@@ -23,3 +87,5 @@ function parseXML(data) {
     });
   });
 }
+
+module.exports = { fetchXML };
